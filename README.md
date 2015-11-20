6dc12c4f82c157c323ab31b5dca3f8f6d2c9a4e3
schau009 <schau009@ucr.edu>
2015-11-20 14:32:27 -0800

    Final Iteration of hw2

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This project is a basic shell written in C++ and acts as a pseudo shell.  It does not fully replicate the processes a basic shell can do, such as the cd command.
 
+#hw1
+
+Objective is to write a command shell called rshell that can print a command and read an executable command.  The goal is to utilize connectors to excecute multiple commands at once.  Could not get connectors working.
+
+#hw2
+
+Objective is you utilize and create the test command to the rshell.  A build-upon from hw1.  Using test or the symbolic equivalent [], we test to find file or directories.  Could not get connectors and parentheses working.
+
 #Bugs:
 
 * Connectors are not supported and will not function correctly
@@ -9,3 +17,5 @@ This project is a basic shell written in C++ and acts as a pseudo shell.  It doe
 * Quotes are not supported and are instead ignored
 * There is a Segmentation Fault when only spaces are inputted and followed by an ENTER key
 * The UP arrow and DOWN arrow keys do no filter through previous commands
+* Did not get the parentheses correctly functioning
+* Parentheses cause an error and will not function
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
-
 
 using namespace std;
 
@@ -15,6 +15,7 @@ using namespace std;
 
 int main()
 {
+	struct stat temp;
 	char line[100];
 	char user_name[256];
 	char host_name[256];
@@ -45,12 +46,73 @@ int main()
 			continue;
 		}
 
-		if(strcmp(cmd, "exit") == 0)
+		else if(strcmp(cmd, "exit") == 0)
 		{
 			exit(1);
 		}
 
-		//Execute command if not exit
+		//Checks for flags with key word test or []
+		else if((strcmp(cmd, "test") == 0) || (strcmp(cmd, "[")) == 0)
+		{
+			cmd = strtok(NULL,delimiter);
+			if(strcmp(cmd, "-d") == 0)
+			{
+				cmd = strtok(NULL,delimiter);
+				if(stat(cmd,&temp) == 0 && S_ISDIR(temp.st_mode))
+				{
+					cout << "Directory exists and is a directory." << endl;
+				}
+
+				else
+				{
+					cout << "Directory does not exist or not a directory." << endl;
+				}
+			}
+
+			else if(strcmp(cmd, "-f") == 0)
+			{
+				cmd = strtok(NULL,delimiter);
+				if(stat(cmd,&temp) == 0 && S_ISREG(temp.st_mode))
+				{
+					cout << "File exists and is a file." << endl;
+				}
+
+				else
+				{
+					cout << "File does not exist or not a file." << endl;
+				}
+			}
+
+			else if(strcmp(cmd, "-e") == 0)
+			{
+				cmd = strtok(NULL,delimiter);
+				if((stat(cmd,&temp) == 0 && S_ISREG(temp.st_mode)) || (stat(cmd,&temp) == 0 && S_ISDIR(temp.st_mode)))
+				{
+					cout << "File/Directory exists." << endl;
+				}
+
+				else
+				{
+					cout << "File/Directory does not exist." << endl;
+				}
+			}
+			
+			else
+			{
+				if((stat(cmd,&temp) == 0 && S_ISREG(temp.st_mode)) || (stat(cmd,&temp) == 0 && S_ISDIR(temp.st_mode)))
+				{
+					cout << "File/Directory exists." << endl;
+				}
+
+				else
+				{
+					cout << "File/Directory does not exist." << endl;
+				}
+			}
+		}
+
+
+		//Execute command if not any of the other commands
 		else
 		{
 			int num = fork();
